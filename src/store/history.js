401284470c3fe103b9fb411b943a8a43a9d7c796
VSsmbrsk <kolhoznikmarek@gmail.com>
2026-01-18 21:31:24 +0200

    fix: ensure basename is set for redux history context

--- src/store/history.js
+++ src/store/history.js
@@ -4,4 +4,5 @@ import { createBrowserHistory } from "history";
 export const { routerReducer, routerMiddleware, createReduxHistory } =
   createReduxHistoryContext({
     history: createBrowserHistory(),
+    basename: "/booking-app",
   });
