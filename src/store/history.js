035a657488b787e47f5418841ec4314baac7e963
VSsmbrsk <kolhoznikmarek@gmail.com>
2026-01-18 22:08:33 +0200

    fix: add missing import statements for redux history context

--- src/store/history.js
+++ src/store/history.js
@@ -1,3 +1,6 @@
+import { createReduxHistoryContext } from "redux-first-history";
+
+import { createBrowserHistory } from "history";
 export const { routerReducer, routerMiddleware, createReduxHistory } =
   createReduxHistoryContext({
     history: createBrowserHistory(),
