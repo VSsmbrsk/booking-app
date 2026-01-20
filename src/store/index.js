25a436fdcb48258f4df35bc767113e03adee0db7
VSsmbrsk <kolhoznikmarek@gmail.com>
2026-01-20 21:19:59 +0200

    fix: add missing compose import for Redux store configuration

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./rootReducer";
 import rootSaga from "./rootSaga";
@@ -9,7 +9,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
   rootReducer,
-  composeEnhancers(applyMiddleware(routerMiddleware, sagaMiddleware))
+  composeEnhancers(applyMiddleware(routerMiddleware, sagaMiddleware)),
 );
 
 export const history = createReduxHistory(store);
