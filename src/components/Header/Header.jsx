2c7329a9147d906fd6ccba5f4d259b717e6ef404
VSsmbrsk <kolhoznikmarek@gmail.com>
2026-01-20 21:49:04 +0200

    fix: remove unnecessary header tags and adjust toolbar padding for responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,20 +4,18 @@ import Logo from "../Logo/Logo";
 
 const Header = () => {
   return (
-    <header>
-      <AppBar position="static" elevation={0} color="transparent">
-        <Toolbar sx={{ justifyContent: "space-between", padding: "0" }}>
-          <Link href="/">
-            <Logo />
-          </Link>
+    <AppBar position="static" elevation={0} color="transparent">
+      <Toolbar sx={{ justifyContent: "space-between", padding: "0" }}>
+        <Link href="/">
+          <Logo />
+        </Link>
 
-          <Box sx={{ display: "flex", gap: 2 }}>
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
-          </Box>
-        </Toolbar>
-      </AppBar>
-    </header>
+        <Box sx={{ display: "flex", gap: 2 }}>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </Box>
+      </Toolbar>
+    </AppBar>
   );
 };
 export default Header;
